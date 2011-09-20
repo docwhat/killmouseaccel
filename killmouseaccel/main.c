d7631b522b6dac2ed1908f52913418896f632653
Christian Höltje <docwhat@gerf.org>
2011-09-20 09:41:54 -0400

    Yay! It works.

--- killmouseaccel/main.c
+++ killmouseaccel/main.c
@@ -3,16 +3,39 @@
 //  killmouseaccel
 //
 //  Created by Christian Höltje on 9/20/11.
-//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//  This is public domain. The code was originally taken from
+//  http://forums3.armagetronad.net/viewtopic.php?t=3364
 //
 
-#include <CoreFoundation/CoreFoundation.h>
+#include <stdio.h>
+#include <IOKit/hidsystem/IOHIDLib.h>
+#include <IOKit/hidsystem/IOHIDParameter.h>
 
-int main (int argc, const char * argv[])
+int main(int argc, char **argv)
 {
-
-    // insert code here...
-    CFShow(CFSTR("Hello, World!\n"));
+    const int32_t accel = -0x10000; // if this ever becomes a scale factor, we set it to one
+    if(argc < 2) {
+        fprintf(stderr, "Give me mouse and/or trackpad as arguments\n");
+        return 1;
+    }
+    
+    io_connect_t handle = NXOpenEventStatus();
+    if(handle) {
+        int i;
+        for(i=1; i<argc; i++) {
+            CFStringRef type = 0;
+            
+            if(strcmp(argv[i], "mouse") == 0)
+                type = CFSTR(kIOHIDMouseAccelerationType);
+            else if(strcmp(argv[i], "trackpad") == 0)
+                type = CFSTR(kIOHIDTrackpadAccelerationType);
+            
+            if(type && IOHIDSetParameter(handle, type, &accel, sizeof accel) != KERN_SUCCESS)
+                fprintf(stderr, "Failed to kill %s accel\n", argv[i]);
+        }
+        NXCloseEventStatus(handle);
+    } else
+        fprintf(stderr, "No handle\n");
+    
     return 0;
 }
-
