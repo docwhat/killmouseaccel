98fba3f031720ddcc583d26db6e5a5661386b84f
Christian Höltje <docwhat@gerf.org>
2011-09-20 09:23:33 -0400

    Initial Commit

---
+++ killmouseaccel/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  killmouseaccel
+//
+//  Created by Christian Höltje on 9/20/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#include <CoreFoundation/CoreFoundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    CFShow(CFSTR("Hello, World!\n"));
+    return 0;
+}
+
